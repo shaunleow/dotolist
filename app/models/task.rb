894fc61c96f77ea403674b28df18206e91293af4
shaunleow <shaunleow@gmail.com>
2017-03-18 10:13:11 +0800

    Added validations to our task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,16 @@
 class Task < ApplicationRecord
 	belongs_to :user
+
+	validates :user_id, presence: true
+	validates :content, presence: true
+
+	auto_html_for :content do 
+		html_escape
+		image
+		vimeo(width: '100%', height: 250)
+		youtube(width: '100%', height: 250)
+		link target: '_blank', rel: 'nofollow' 
+		simple_format
+	end
 end
+
